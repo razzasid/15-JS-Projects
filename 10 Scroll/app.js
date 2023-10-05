dd6002bcb9dcc0f7a6d450b6795776a99df53153
razzasid <razamohd6395@gmail.com>
2023-10-05 12:49:31 +0530

    clear space

--- 10 Scroll/app.js
+++ 10 Scroll/app.js
@@ -26,7 +26,7 @@ navToggle.addEventListener("click", function () {
 });
 
 const navbar = document.getElementById("nav");
-const topLink = document.querySelector(".top-link   ");
+const topLink = document.querySelector(".top-link");
 // ********** fixed navbar ************
 window.addEventListener("scroll", function () {
   const scrollHeight = window.pageYOffset;
